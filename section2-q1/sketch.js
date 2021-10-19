4f92e991b39782500f07c0dc77e744a9149daa3c
hidamariphant <92303611+hidamariphant@users.noreply.github.com>
2021-10-19 14:09:33 +0900

    section1-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,26 @@
 // 小手調べ
 function setup(){
+　noFill();//塗りつぶしなし
+　ellipse(50,50,10)
+  ellipse(50,50,20)
+  ellipse(50,50,30)
+  ellipse(50,50,40)
+
   for(let i = 0; i < 10; i++){
-    // BLANK[1]
+   //let r = ???
+   let r = i * 10 + 10;
+   if(i < 5){
+   }
+   else{
+
+   }
+
+   ellipse(50, 50, r)
+
+
   }
 }
+
+
+//まず10本の線で円を描く
+//その後、条件で分けて赤と青の円をそれぞれ描く（ifとelse）
--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -2,10 +2,18 @@
 function setup() {
   createCanvas(200, 200);
   let size = width / 8;
-  noStroke();
+  //noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+　　rect(size * i,)
+
     }
   }
 }
+
+
+//中間ゴールは白黒のチェッカーをつくること。
+//二重ループのプログラムの応用
+
+//条件で分ける
